1f04c3bf361fb0daafe18af150fc32a4e63b4b7c
Sebastian Wabnik <sebastian.wabnik@enova.pl>
2016-05-09 15:19:54 +0100

    Workery obsługujące dodawanie wiadomości na Dokumencie

---
+++ WiadomosciExt/DodajWiadomoscWorker.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using Soneta.Business;
+using Soneta.Examples.PocztaExt.WiadomosciExt;
+using Soneta.Handel;
+using Soneta.Business.Licence;
+using Soneta.Core;
+using Soneta.CRM;
+using Soneta.Commands;
+
+[assembly: Worker(
+    typeof(DodajWiadomoscWorker),
+    typeof(DokumentHandlowy))]
+
+namespace Soneta.Examples.PocztaExt.WiadomosciExt
+{
+    public class DodajWiadomoscWorker
+    {   
+        [Context]
+        public DokumentHandlowy Dokument { get; set; }
+
+        [Action("Nowa wiadomość", Target = ActionTarget.ToolbarWithText, Mode = ActionMode.SingleSession,
+        CommandShortcut = CommandShortcut.F8, Priority = 10,
+        Contexts = new object[] { LicencjeModułu.CRM | LicencjeModułu.HAN })]
+        public WiadomoscRobocza NowaWiadomosc()
+        {
+            WiadomoscRobocza Wiadomosc;
+            using (ITransaction t = Dokument.Session.Logout(true))
+            {
+                Wiadomosc = new WiadomoscRobocza();
+                CRMModule.GetInstance(Dokument).WiadomosciEmail.AddRow(Wiadomosc);
+                if (Dokument.Kontrahent != null)
+                {
+                    Wiadomosc.Temat += "[" + Dokument.Numer + "]";
+                    Wiadomosc.Tresc += "Witaj," + System.Environment.NewLine + Dokument.Kontrahent.NazwaFormatowana;
+                    if (Dokument.Kontrahent.EMAIL != null) Wiadomosc.Do = Dokument.Kontrahent.EMAIL;
+
+                }
+
+                var WiadomoscDokumentu = new WiadomoscExt(Wiadomosc, Dokument);
+                PocztaExtModule.GetInstance(Dokument).WiadomosciExt.AddRow(WiadomoscDokumentu);
+                t.CommitUI();
+            }
+            return Wiadomosc;
+        }
+    }
+}
---
+++ WiadomosciExt/DokumentyWiadomosciWorker.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using Soneta.Business;
+using Soneta.Examples.PocztaExt.WiadomosciExt;
+using Soneta.Handel;
+using Soneta.CRM;
+
+[assembly: Worker(
+    typeof(DokumentyWiadomosciWorker),
+    typeof(WiadomoscEmail))]
+
+namespace Soneta.Examples.PocztaExt.WiadomosciExt
+{
+    public class DokumentyWiadomosciWorker
+    {
+        [Context]
+        public WiadomoscEmail Wiadomosc { get; set; }
+
+        [Description("Lista dokumentów powiązanych z wiadomością Email.")]
+        public SubTable Dokumenty
+        {
+            get
+            {
+                var WiadomosciModule = PocztaExtModule.GetInstance(Wiadomosc);
+                return WiadomosciModule.WiadomosciExt.WgWiadomosc[Wiadomosc];
+            }
+        }
+    }
+}
