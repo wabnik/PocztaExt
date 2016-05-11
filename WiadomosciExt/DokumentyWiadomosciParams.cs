3213319bdb480bcdcad0c264874e39da13110d8d
Tomasz Schabowski <tomasz.schabowski@enova.pl>
2016-05-11 15:20:04 +0200

    ViewInfo

---
+++ WiadomosciExt/DokumentyWiadomosciParams.cs
@@ -0,0 +1,43 @@
+﻿using Soneta.Business;
+using Soneta.Handel;
+using Soneta.Types;
+
+namespace Soneta.Examples.PocztaExt.WiadomosciExt
+{
+    public class DokumentyWiadomosciParams : ContextBase
+    {
+        private const string Key = "Soneta.PocztaExt.DokumentyWiadomosci";
+
+        public DokumentyWiadomosciParams(Context cx) : base(cx)
+        {
+            Load();
+        }
+
+        [Context]
+        [Caption("Definicja")]
+        public DefDokHandlowego Definicja
+        {
+            get
+            {
+                if (Context.Contains(typeof(DefDokHandlowego)))
+                    return (DefDokHandlowego)Context[typeof(DefDokHandlowego)];
+                return null;
+            }
+            set
+            {
+                Context[typeof(DefDokHandlowego)] = value;
+                Save();
+            }
+        }
+
+        protected void Load()
+        {
+            SetContext(typeof(DefDokHandlowego), Session.Login.Load(this, Key, "Definicja"));
+        }
+
+        protected void Save()
+        {
+            Session.Login.Save(this, Key, "Definicja");
+        }
+    }
+}
---
+++ WiadomosciExt/DokumentyWiadomosciViewInfo.cs
@@ -0,0 +1,57 @@
+﻿using Soneta.Business;
+using Soneta.Business.Licence;
+using Soneta.Business.UI;
+using Soneta.Examples.PocztaExt.WiadomosciExt;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+[assembly: FolderView("Handel/Dokumenty wysłane",
+    Priority = 1000,
+    Description = "Dokumenty wysłane",
+    BrickColor = FolderViewAttribute.BlueBrick,
+    TableName = "DokHandlowe",
+    ViewType = typeof(DokumentyWiadomosciViewInfo),
+    Contexts = new object[] { LicencjeModułu.All }
+)]
+
+namespace Soneta.Examples.PocztaExt.WiadomosciExt
+{
+    public class DokumentyWiadomosciViewInfo : ViewInfo
+    {
+        public DokumentyWiadomosciViewInfo()
+        {
+            //View wiążemy z odpowiednią definicją viewform.xml 
+            ResourceName = "DokumentyWiadomosci";
+
+            //Inicjowanie contextu
+            InitContext += DokumentyWiadomosciViewInfo_InitContext;
+
+            //Tworzenie view zawerającego 
+            CreateView += DokumentyWiadomosciViewInfo_CreateView;
+        }
+
+        private void DokumentyWiadomosciViewInfo_InitContext(object sender, ContextEventArgs args)
+        {
+            args.Context.TryAdd(() => new DokumentyWiadomosciParams(args.Context));
+        }
+
+        private void DokumentyWiadomosciViewInfo_CreateView(object sender, CreateViewEventArgs args)
+        {
+            DokumentyWiadomosciParams @params;
+            if (!args.Context.Get(out @params))
+                return;
+
+            args.View = PrepareView(@params);
+        }
+
+        private View PrepareView(DokumentyWiadomosciParams @params)
+        {
+            RowCondition cond = new RowCondition.Exists("WiadomosciExt", "Dokument");
+
+            return HandelModule.GetInstance(@params).DokHandlowe.WgDefinicja[@params.Definicja][cond].CreateView();
+        }
+    }
+}
